8ea93b9bc3b1c59f660cb06265863ee0d78c710c
Artur Barbsoa <artur.barbosa@gmail.com>
2017-05-03 20:50:34 -0300

    Versão inicial do projeto

---
+++ Loja/src/MainApp.java
@@ -0,0 +1,13 @@
+import edu.univas.si4.mvc.MainFrame;
+
+
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new MainFrame().setVisible(true);
+	}
+
+}
